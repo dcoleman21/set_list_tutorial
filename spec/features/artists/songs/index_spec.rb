01deead4ee9cac8285af9980b53e68db6d91f3b1
Dani Coleman <danicoleman00@gmail.com>
2022-02-01 17:39:11 -0700

    Add feature tests to see an artists songs, link to song show page, average length of song

---
+++ spec/features/artists/songs/index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Artists songs index' do
+  before(:each) do
+    @prince = Artist.create!(name: "Prince")
+    @purple = @prince.songs.create!(title: "Purple Rain", length: 854, play_count: 8599)
+    @beret  = @prince.songs.create!(title: "Raspberry Beret", length: 878, play_count: 9034)
+    @doves  = @prince.songs.create!(title: "When Doves Cry", length: 1, play_count: 9934)
+  end
+
+  it "shows all of the songs for the artist" do
+
+    visit "/artists/#{@prince.id}/songs"
+
+    expect(page).to have_content(@purple.title)
+    expect(page).to have_content(@beret.title)
+  end
+
+  it "links to each songs show page" do
+    visit "/artists/#{@prince.id}/songs"
+
+    click_on @purple.title
+
+    expect(current_path).to eq("/songs/#{@purple.id}")
+  end
+
+  it "shows the average song length for the artist" do
+    visit "/artists/#{@prince.id}/songs"
+
+    expect(page).to have_content("Average Song Length for Prince: #{@prince.average_song_length}")
+  end
+end
